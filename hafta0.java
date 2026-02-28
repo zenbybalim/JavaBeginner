d6a7b6d722e8278c5f7ddd05a703cb478370835d
Balım Çetin <elifbalimcetin@gmail.com>
2026-02-28 18:23:04 +0300

    Add week0.java class file

--- hafta0.java
+++
@@ -1,3 +0,0 @@
-public class hafta0 {
-    
-}
---
+++ week0.java
@@ -0,0 +1,8 @@
+public class week0 { 
+
+    public static void main(String[] args) { 
+        
+        System.out.println("Hello Java"); 
+        
+    } 
+}
