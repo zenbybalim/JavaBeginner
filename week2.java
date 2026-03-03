7ff3da2c11c8a0ebdb6b10f8f2874a0e8e2397ca
Balım Çetin <elifbalimcetin@gmail.com>
2026-03-03 20:10:58 +0300

    Add week2.java class file for salary calculation based on hours worked

--- week2.java
+++ week2.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class week2 {
-    
+public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double saat, maas1, maas2, sigorta, vergi;
+
+        System.out.println("Çalişan kaç saat çalisti");
+        saat = input.nextDouble();
+
+        maas1 = saat*100;
+        sigorta = maas1*0.15;
+        vergi = maas1*0.10;
+
+        maas2 = maas1 - (sigorta + vergi);
+
+        System.out.println("Brüt maas=" +maas1);
+        System.out.println("Sigorta=" +sigorta);
+        System.out.println("vergi=" +vergi);
+        System.out.println("net maas=" +maas2);
+
+}
 }
