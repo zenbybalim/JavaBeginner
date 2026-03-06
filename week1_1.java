64ecf8f78d3430f899e78d567365cc32eac8a525
Balım Çetin <elifbalimcetin@gmail.com>
2026-03-06 12:19:07 +0300

    Add week1_1.java and week2_1.java class files for sum and salary calculations

--- week1_1.java
+++ week1_1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class week1 {
+public class week1_1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
--- week2_1.java
+++ week2_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class week2 {
+public class week2_1 {
 public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
