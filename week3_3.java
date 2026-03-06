7851870a67584979373a5b8b83afbcf841727c1b
Balım Çetin <elifbalimcetin@gmail.com>
2026-03-06 15:47:30 +0300

    Add week3_3.java class file for quadratic equation solver

---
+++ week3_3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class week3_3 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        double a, b, c;
+        double d1, d2, x1, x2;
+
+        System.out.println("For ax^2 + bx + c, a =");
+        a = input.nextDouble();
+
+        System.out.println("b = ");
+        b = input.nextDouble();
+
+        System.out.println("c = ");
+        c= input.nextDouble();
+
+        d1 = (b*b - 4*a*c);
+
+        d2 = Math.sqrt((b*b - 4*a*c));
+
+        x1 = (- b + d2)/(2*a);
+        x2 = (- b - d2)/(2*a);
+
+        System.out.println("x1 =" +x1);
+        System.out.println("x2= " +x2);
+        System.out.println("Delta : " +d1);
+
+    }
+    
+}
