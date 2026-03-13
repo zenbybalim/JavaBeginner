3c8e081c8f9ebf217e3e7fef7e94f647ec624cd8
Balım Çetin <elifbalimcetin@gmail.com>
2026-03-13 19:16:33 +0300

    carpim3

--- week4_practices_java/week4_907.java
+++ week4_practices_java/week4_907.java
@@ -3,12 +3,12 @@ package week4_practices_java;
 public class week4_907 {
     public static void main(String[] args){
 
-        for(int carpan1=1; carpan1<3;carpan1++){
+        for(int carpan1=1; carpan1<=2;carpan1++){
             for(int carpan2= 1; carpan2<3; carpan2++){
                 int sonuc = carpan1 *carpan2;
                 System.out.println(+carpan1 + "x" + carpan2 +"=" +sonuc);
             }
-            System.out.println("_-*-_");
+            System.out.println("_-_");
         }
     }
     
