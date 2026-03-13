ac02491061c6ced295ae2e86ebc4ba583c921e5a
Balım Çetin <elifbalimcetin@gmail.com>
2026-03-13 11:53:23 +0300

    Add VAT calculator based on product categories using switch-case logic.

--- week3_practices_java/week3_916.java
+++ week3_practices_java/week3_916.java
@@ -6,5 +6,43 @@ import java.util.Scanner;
 // (Market için M, Yeme-içme için Y ve diğer ürünler için D bilgisinin girildiğini varsayınız.)
 
 public class week3_916 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+    String urun;
+    Double fiyat=0.0, kdv=0.0, toplam=0.0;
+
+     System.out.println("Hangi market urununu aldiniz : Market icin M, Yeme-icme icin Y ve diger urunler icin D");
+     urun = input.next();
+     
+     switch(urun){
+        case "m":
+        case "M":
+            kdv = 0.01;
+            break;
+        case "y":
+        case "Y":
+            kdv = 0.1;
+            break;
+        case "d":
+        case "D":
+            kdv = 0.2;
+            break;
+        default: 
+        System.out.println("Hata: Gecersiz urun bilgisi girdiniz!");        
+     }
+     if(urun.equals("m")||urun.equals("M")|| urun.equals("y")|| urun.equals("Y")||urun.equals("d")||urun.equals("D")){
+      //if(kdv>0){} !!!seklinde de yazabilirim.
+        System.out.println("Aldiginiz urunun fiyatini giriniz = ");
+        fiyat = input.nextDouble();
+
+        toplam = fiyat + (fiyat * kdv);
+        kdv = fiyat * kdv;
+
+        System.out.println("odemeniz gereken KdV tuttari = " +kdv);
+        System.out.println("Odemeniz gereken toplam tutar = " +toplam);
+     
+     }
+    }
     
 }
