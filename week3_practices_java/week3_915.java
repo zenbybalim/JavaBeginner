e4c05e53dcf2c3221ddb501e5fa4b10807b760da
Balım Çetin <elifbalimcetin@gmail.com>
2026-03-13 11:16:31 +0300

    Add week3_915.java class file to determine student eligibility for course enrollment based on specific grade criteria

---
+++ week3_practices_java/week3_915.java
@@ -0,0 +1,56 @@
+package week3_practices_java;
+import java.util.Scanner;
+//Aşağıdaki ölçütleri temel alarak öğrencinin bir kursa kabul edilmeye uygun olup olmadığını belirleyen programı yazınız.
+// Şartlar şu şekildedir;
+//Öğrencinin Matematik Notu >=65 olmalı,
+//Öğrencinin Fizik Notu >=55 olmalı,
+//Öğrencinin Kimya Notu >=50 olmalı,
+//Her üç dersten notları toplam >=190 olmalı veya Matematik ve Fizik Toplamı >=140 olmalı
+//Bu işlem gerçekleştiren programın kodlarını yazınız.
+public class week3_915 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        Double mat, fiz, kim, fizvemat=0.0, toplam=0.0;
+
+        System.out.println("Matematik notunuzu giriniz : ");
+        mat = input.nextDouble();
+
+        System.out.println("Fizik notunuzu giriniz : ");
+        fiz = input.nextDouble();
+
+        System.out.println("Kimya notunuzu giriniz : ");
+        kim = input.nextDouble();
+
+        toplam = mat + fiz + kim ;
+        fizvemat = mat + fiz;
+
+        if(mat >= 65){
+            if(fiz >= 55){
+                if(kim >= 50){
+                    if(fizvemat >=140){
+                        if(toplam >= 190){
+                            System.out.println("Ogrenci kursa katilmaya uygun.");
+                        }
+                        else{
+                            System.out.println("Ogrenci kursa katilmaya uygun degil.");
+                        }
+                    }
+                    else{
+                            System.out.println("Ogrenci kursa katilmaya uygun degil.");
+                        }
+                }
+                else{
+                            System.out.println("Ogrenci kursa katilmaya uygun degil.");
+                        }
+            }
+            else{
+                            System.out.println("Ogrenci kursa katilmaya uygun degil.");
+                        }
+        }
+        else{
+                            System.out.println("Ogrenci kursa katilmaya uygun degil.");
+                        }
+    }
+    
+}
