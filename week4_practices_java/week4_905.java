8c03c43fd66f857499f821f38c116ea1fdb49e4c
Balım Çetin <elifbalimcetin@gmail.com>
2026-03-13 16:55:35 +0300

    tahmin

---
+++ week4_practices_java/week4_905.java
@@ -0,0 +1,29 @@
+package week4_practices_java;
+import java.util.Scanner;
+import java.util.Random;
+
+public class week4_905 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        Random rndm = new Random();
+
+        int tahmin, sayi;
+
+        sayi = rndm.nextInt(100);
+        System.out.println("Tutulan sayiyi bul.");
+
+        do{
+            System.out.print("Tahmininizi giriniz = ");
+            tahmin = input.nextInt();
+
+            if(tahmin<sayi)
+                System.out.println("daha yüksek bir deger girin!");
+            else if(tahmin>sayi)
+                System.out.println("Daha dusuk bir deger girin!");
+            else if (tahmin==sayi)
+                System.out.println("Dogru Tahmin!");
+        }
+        while(tahmin != sayi);
+    }
+    
+}
